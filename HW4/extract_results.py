2ac395d2a6637334ee1129ab22de56721c244991
proguorammer <yueguo598@gmail.com>
2018-11-19 21:08:14 -0500

    kaggle 0.63 valid accuracy
    used 15 epoches, VGG, preprocessing

---
+++ HW4/extract_results.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+import csv
+
+currPath = os.getcwd()
+with open(os.path.join(currPath, 'test_predicted.pickle'), 'rb') as jar:
+  y_test = pickle.load(jar)
+
+
+# get the one - to one mapping for labels to one hot
+table_labels = pd.read_csv('./all/train_labels.csv')
+label_list = table_labels.Category.unique().tolist()
+print(label_list)
+
+
+# from one hot encoding back to the category name for test pickle
+y_classes = [label_list[np.argmax(y, axis=None, out=None)] for y in y_test]
+
+# build csv
+with open('test_predicted.csv', 'w') as csvfile:
+    # defined by the sample csv
+    fieldnames = ['Id', 'Category']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for i in range(len(y_classes)):
+        writer.writerow({'Id':i, 'Category':y_classes[i]})
